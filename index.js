0b708727e08715f80b5518fdb75cf42a286aadfe
huangminguang <15896938825@163.com>
2022-07-27 13:43:54 +0800

    link module

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: TerryMin
+ * @Date: 2022-07-27 13:33:26
+ * @LastEditors: TerryMin
+ * @LastEditTime: 2022-07-27 13:40:07
+ * @Description: file not
+ */
+module.exports={
+  name:"submodule-utils",
+  sayHello:function(){
+    console.log('hello submodule-utils test');
+  }
+}
